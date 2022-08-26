8548bf2acb45b1db730f316ba537d0303c10634a
Marlon <Marlonnlmx@outlook.com>
2022-08-26 12:12:15 -0300

    Updating the code

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,6 +1,10 @@
-export class Post {
-  public readonly id: string;
-  public title: string;
-  public text: string;
-  public imagefile?: string;
+import { Post } from "@prisma/client";
+
+export class PostEnity implements Post {
+  id: string;
+  title: string;
+  text: string;
+  imagefile: string;
+  created_at: Date;
+  updated_at: Date;
 }
