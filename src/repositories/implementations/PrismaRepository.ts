0691f7059fa8f592f34b768f57dc06acfe4de3a9
Marlon <Marlonnlmx@outlook.com>
2022-09-05 14:27:43 -0300

    Implementing new methods and formatting code

--- src/repositories/implementations/PrismaRepository.ts
+++ src/repositories/implementations/PrismaRepository.ts
@@ -1,30 +1,54 @@
-
-import { ICreatePostDTO } from "../../dto/ICreatePostDTO";
-import { IUpdatePostDTO } from "../../dto/IUpdatePostDTO";
-import { Post } from "../../entities/Post";
+import { ICreatePostDTO } from "../../useCases/create/ICreatePostDTO";
+import { IUpdatePostDTO } from "../../useCases/edit/IUpdatePostDTO";
+import { PostEnity } from "../../entities/Post";
 import { prisma } from "../../prisma";
 import { IPostRepository } from "../IPostRepository";
 
 export class PrismaRepository implements IPostRepository {
-  async create({ title, text, imagefile }: ICreatePostDTO): Promise<void> {
+  async create({ title, text, imagefile }: ICreatePostDTO): Promise<PostEnity> {
     return await prisma.post.create({
       data: {
         title,
         text,
         imagefile,
-      }
-    })
+      },
+    });
   }
 
-  async update({ id, title, text, imagefile }: IUpdatePostDTO): Promise<Post> {
+  async findById(id: string): Promise<PostEnity | null> {
+    return await prisma.post.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async findByTitle(title: string): Promise<PostEnity[] | null> {
+    return await prisma.post.findMany({
+      where: {
+        title
+      }
+    });
+  }
+
+  async findAll(): Promise<PostEnity[]> {
+    return await prisma.post.findMany();
+  }
+
+  async update({
+    id,
+    title,
+    text,
+    imagefile,
+  }: IUpdatePostDTO): Promise<PostEnity> {
     return await prisma.post.update({
       where: { id },
       data: {
         title,
         text,
         imagefile,
-      }
-    })
+      },
+    });
   }
 
   async delete(id: string) {
