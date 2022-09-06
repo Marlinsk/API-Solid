2092318046e7bedf7b5cf6bc02c1b1ae25b68e0a
Marlon <Marlonnlmx@outlook.com>
2022-09-06 13:24:29 -0300

    Removing unnecessary things

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,6 @@ import { createPostController } from "./useCases/create";
 import { deletePostController } from "./useCases/delete";
 import { editPostController } from "./useCases/edit";
 import { findAllPostsController } from "./useCases/read/find-all";
-import { findPostByTitleController } from "./useCases/read/find-by-title";
 
 const router = Router();
 
@@ -22,9 +21,5 @@ router.put("/edit/post/:id", async (request, response) => {
 router.delete("/delete/post/:id", async (request, response) => {
   return deletePostController.handle(request, response);
 });
-
-// router.get("/find-by-title/:title", async (request, response) => {
-//   return findPostByTitleController.handle(request, response);
-// });
 
 export { router };
